11c5e2e57cf70d6a77226e7c462ab199f61ab5be
Ankit <ankitnashier91@gmail.com>
2020-02-25 22:52:54 +0530

    Created gh-pages branch

---
+++ build/precache-manifest.81c3df9a794fb2bcc420a6ae91338f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0370930c8ec77b79488e28f28048397f",
+    "url": "/cards-shuffle/index.html"
+  },
+  {
+    "revision": "69b3c51f46bf4b279e07",
+    "url": "/cards-shuffle/static/css/main.a136b419.chunk.css"
+  },
+  {
+    "revision": "88d068037be87e8cb481",
+    "url": "/cards-shuffle/static/js/2.eb97ba2f.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/cards-shuffle/static/js/2.eb97ba2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b3c51f46bf4b279e07",
+    "url": "/cards-shuffle/static/js/main.1e61decb.chunk.js"
+  },
+  {
+    "revision": "7a195d04f4756de4d2c7",
+    "url": "/cards-shuffle/static/js/runtime-main.b0d8c3ce.js"
+  }
+]);
